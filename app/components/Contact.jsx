ff3a1ee9e6db23ae33d9c01eaf02c7b195057a9b
Abhishek-raj093 <ss0986825@gmail.com>
2025-01-12 11:48:12 +0530

    Final Commit

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,27 +1,100 @@
-import React from 'react'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { motion } from "motion/react"
 
 const Contact = () => {
-  return (
-    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/ footer-bg-color.png")] bg-no-repeat bg-center bg-[lenght:90%_auto]'>
-       <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
-       <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
 
-       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "6bf72f08-9e54-480c-9865-a6a2e892f4d8");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}} 
+    id='contact' className='w-full px-[12%] py-10 scroll-mt-20 '>
+
+       <motion.h4 
+       initial={{y: -20, opacity: 0}}
+       whileInView={{y: 0, opacity: 1}}
+       transition={{duration: 0.5, delay: 0.3}}
+       className='text-center mb-2 text-lg font-Ovo'>Connect with me</motion.h4>
+
+       <motion.h2 
+       initial={{y: -20, opacity: 0}}
+       whileInView={{y: 0, opacity: 1}}
+       transition={{duration: 0.5, delay: 0.5}}
+       className='text-center text-5xl font-Ovo'>Get in touch</motion.h2>
+
+       <motion.p 
+       initial={{opacity: 0}}
+       whileInView={{opacity: 1}}
+       transition={{duration: 0.5, delay: 0.7}}
+       className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
         I'd love to hear from you! If you have any questions, comments, or feedback, 
         please use the form below.
-       </p>
+       </motion.p>
 
-       <form className='max-w-2xl mx-auto'>
+       <motion.form 
+       initial={{opacity: 0}}
+       whileInView={{opacity: 1}}
+       transition={{duration: 0.5, delay: 0.9}}
+       onSubmit={onSubmit} className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-            <input type="text" placeholder='Enter your name' required
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
-            <input type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
-        </div>
-        <textarea rows='6' placeholder='Enter your message' required className='flex-1 p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
 
-        <button type='submit'>Submit now</button>
-       </form>
-    </div>
+            <motion.input 
+            initial={{x: -50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 0.6, delay: 1.1}}
+            type="text" placeholder='Enter your name' required
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name'/>
+
+            <motion.input 
+            initial={{x: 50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 0.6, delay: 1.2}}
+            type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email'/>
+
+        </div>
+        <motion.textarea 
+        initial={{y: 100, opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{duration: 0.6, delay: 1.6}}
+        rows='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></motion.textarea>
+
+        <motion.button 
+        whileInView={{scale: 1.05}}
+        transition={{duration: 0.3}}
+        type='submit'
+        className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>
+          Submit now <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.button>
+
+          <p className='mt-4'>{result}</p>
+       </motion.form>
+    </motion.div>
   )
 }
 
